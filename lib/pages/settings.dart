d1802152c518600d58635b51a0737c1b829699b9
george redinger <george@georgeredinger.com>
2019-02-04 17:31:14 -0800

    right hand side side Drawer 'endDrawer'

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
 
-import './sensorsettings.dart';
+import './settingssensor.dart';
 
 class SettingsPage extends StatelessWidget {
-  ListTile setter(BuildContext context,icon, String maintext, subtext) {
+  ListTile setter(BuildContext context, icon, String maintext, subtext) {
     return ListTile(
-      leading: IconButton(icon:Icon(icon), color: Colors.red.shade400,
-          onPressed: () {
-        Navigator.push(
-            context,
-            MaterialPageRoute(
-                builder: (BuildContext context) => Sensor()));
-      }),
+      onLongPress: () {
+        print(subtext);
+      },
+      onTap: () {
+        print(maintext);
+      },
+      leading: IconButton(
+          icon: Icon(icon),
+          color: Colors.red.shade400,
+          onPressed: () {//this should be in the onTap() of the ListTile widget to allow tapping any part of the "tile"
+            Navigator.push(context,
+                MaterialPageRoute(builder: (BuildContext context) => Sensor()));
+          }),
       trailing: Icon(
         Icons.expand_more,
         color: Colors.blue,
@@ -31,14 +37,15 @@ class SettingsPage extends StatelessWidget {
         title: Text('Settings'),
       ),
       body: ListView(children: [
-        setter(context,Icons.add_circle, "Emergency Contacts", "We hope you are OK"),
-        setter(context,Icons.directions_bike, "Cyclists",
+        setter(context, Icons.add_circle, "Emergency Contacts", //Emergency()
+            "We hope you are OK"),
+        setter(context, Icons.directions_bike, "Cyclists",
             "Find out where they are before you ride"),
-        setter(context,Icons.settings_remote, "Sensor", "Feel the motion"),
-        setter(context,Icons.security, "Privacy",
+        setter(context, Icons.settings_remote, "Sensor", "Feel the motion"),//Sensor()
+        setter(context, Icons.security, "Privacy",
             "Not posted to Facebook, but we do keep it, for \"safety\""),
-        setter(context,Icons.portrait, "My Info", "Don't worry we won't tell"),
-        setter(context,Icons.room_service, "Notificatons",
+        setter(context, Icons.portrait, "My Info", "Don't worry we won't tell"),
+        setter(context, Icons.room_service, "Notificatons",
             "Ya know you want people to notice"),
       ]),
     );
